081a0467798175f7c66cbf47d0b89388d1ef1151
janishatech <janisha1508@gmail.com>
2024-03-15 15:58:01 +0530

    feature:report of sales and purchase daily items count

--- amilma_custom/amilma_custom/report/sku_wise__daily_sales_in_box/sku_wise__daily_sales_in_box.py
+++ amilma_custom/amilma_custom/report/sku_wise__daily_sales_in_box/sku_wise__daily_sales_in_box.py
@@ -2,6 +2,7 @@
 # # Copyright (c) 2024, Vivek and contributors
 # # For license information, please see license.txt
 
+
 # import frappe
 # from frappe.utils import getdate
 # from datetime import datetime, timedelta
@@ -57,19 +58,19 @@
 #             {
 #                 "fieldname": date,
 #                 "label": f"<b>{date}</b>",
-#                 "fieldtype": "int",
+#                 "fieldtype": "Float",
 #                 "width": 100
 #             }
 #         )
-#     columns.append(
-#         {
-#             "fieldname": "total_quantity",
-#             "label": "<b>Total Quantity</b>",
-#             "fieldtype": "int",
-#             "default": 0,
-#             "width": 150,
-#         }
-#     )
+    
+#     total_quantity_column = {
+#         "fieldname": "total_quantity",
+#         "label": "<b>Total Quantity</b>",
+#         "fieldtype": "Float",
+#         "default": 0,
+#         "width": 150,
+#     }
+#     columns.append(total_quantity_column)
 
 #     # Organizing data into a dictionary for easy manipulation
 #     final_data = {}
@@ -78,17 +79,17 @@
 #         if item_code not in final_data:
 #             final_data[item_code] = {date: 0 for date in formatted_dates}
 #         final_data[item_code][row['posting_date']] = row['quantity']
-#         # frappe.errprint(row['quantity'])
 
 #     # Formatting data into the desired structure
 #     formatted_data = []
 #     for item_code, values in final_data.items():
 #         total_quantity = sum(values.values())
-#         frappe.errprint (values)
-#         row = {'item_code': item_code, 'item_name': data[0]['item_name'], 'total_quantity': total_quantity}
+#         row = {'item_code': item_code, 'item_name': data[6]['item_name'], 'total_quantity': total_quantity}
+#         frappe.errprint(data[0]['item_name'])
 #         row.update(values)
 #         formatted_data.append(row)
-
+    
+    
 #     return columns, formatted_data
 
 
@@ -121,10 +122,10 @@ def execute(filters=None):
             s.docstatus = 1
             AND s.is_return = 0
             AND s.company = '{company}'
-            AND s.posting_date BETWEEN '{from_date}' AND '{to_date}'
+            AND DATE_FORMAT(s.posting_date, '%%d/%%b') IN ({', '.join(['%s']*len(formatted_dates))})
         GROUP BY
             si.item_code, si.item_name, posting_date
-    """, as_dict=True)
+    """, tuple(formatted_dates), as_dict=True)
 
     # Constructing columns
     columns = [
@@ -173,9 +174,12 @@ def execute(filters=None):
     formatted_data = []
     for item_code, values in final_data.items():
         total_quantity = sum(values.values())
-        row = {'item_code': item_code, 'item_name': data[6]['item_name'], 'total_quantity': total_quantity}
-        frappe.errprint(data[0]['item_name'])
-        row.update(values)
+        row = {'item_code': item_code, 'item_name': data[0]['item_name'], 'total_quantity': total_quantity}
+        for date in formatted_dates:
+            row[date] = values.get(date, 0)
+            # Add item against quantity below the respective date
+            if date == datetime.now().strftime("%d/%b"):
+                row[date + '_item'] = f"{item_code}: {values.get(date, 0)}"
         formatted_data.append(row)
     
     
